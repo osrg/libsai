eaf4f9391f2464e8c08c1aebf753ab59db453bed
Soramichi Akiyama <akiyama@m.soramichi.jp>
2015-06-26 18:28:35 +0900

    add storage layer to sai_switch_{get|set}_attribute

--- impl/impl_sai_switch.c
+++ impl/impl_sai_switch.c
@@ -15,39 +15,61 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "impl_sai_switch.h"
+#include <string.h>
 
+#include "impl_sai_switch.h"
+#include "impl_sai_util.h"
 #include "../rocker/sai_rocker.h"
 #include "../rocker/sai_rocker_fdb.h"
 
+#define NO_DATA_PLANE
+
+struct __switch {
+  sai_vlan_id_t default_port_vlan_id;
+  sai_mac_t src_mac_address;
+};
+
+static struct __switch* sw;
 extern struct __port_list* global_plist;
 sai_switch_api_t rocker_sai_switch_api;
 
+static int get_port_number(){
+  return rocker_get_port_number();
+}
+
 sai_status_t impl_get_switch_attribute(_In_ uint32_t attr_count, _Inout_ sai_attribute_t *attr_list){
-  int i, n_ports;
+  int i, port_number;
 
   // data plane access
-  n_ports = rocker_get_port_number();
+  port_number = get_port_number();
   
   for(i=0;i<attr_count;i++){
     switch (attr_list[i].id){
     case SAI_SWITCH_ATTR_PORT_NUMBER:
-      attr_list[i].value.u32 = n_ports;
+      attr_list[i].value.u32 = port_number;
       goto next_attr;
-      
+
     case SAI_SWITCH_ATTR_PORT_LIST:
-      if(attr_list[i].value.objlist.count < n_ports){
-	attr_list[i].value.objlist.count = n_ports;
+      if(attr_list[i].value.objlist.count < port_number){
+	attr_list[i].value.objlist.count = port_number;
 	return SAI_STATUS_BUFFER_OVERFLOW;
       }
       else{
 	int j;
-	attr_list[i].value.objlist.count = n_ports;
-	for(j=0;j<n_ports;j++){
+	attr_list[i].value.objlist.count = port_number;
+	for(j=0;j<port_number;j++){
 	  attr_list[i].value.objlist.list[j] = (uint64_t)j;
 	}
 	goto next_attr;
       }
+
+    case SAI_SWITCH_ATTR_DEFAULT_PORT_VLAN_ID:
+      attr_list[i].value.u16 = sw->default_port_vlan_id;
+      goto next_attr;
+
+    case SAI_SWITCH_ATTR_SRC_MAC_ADDRESS:
+      memcpy(attr_list[i].value.mac, sw->src_mac_address, 6);
+      goto next_attr;
 
     default:
       fprintf(stderr, "Error: value %d of sai_port_attr_t is not supported\n", attr_list[i].id);
@@ -58,6 +80,35 @@ sai_status_t impl_get_switch_attribute(_In_ uint32_t attr_count, _Inout_ sai_att
   }
 
   // this function succeedes if (and only if) no error happens on the way.
+  return SAI_STATUS_SUCCESS;
+}
+
+sai_status_t sai_set_switch_attribute(_In_ const sai_attribute_t *attr){
+  sai_vlan_id_t vlan_id;
+
+  switch(attr->id){
+  case SAI_SWITCH_ATTR_DEFAULT_PORT_VLAN_ID:
+    vlan_id = attr->value.u16;
+
+    if(vlan_id_range_ok(vlan_id)){
+      sw->default_port_vlan_id = vlan_id;
+      // should we move the ports belonging to the old default vlan to the new default vlan?
+    }
+    else{
+      fprintf(stderr, "vlan_id (%d) must satisfy 1 <= vlan_id <= 4095\n", vlan_id);
+      return SAI_STATUS_INVALID_VLAN_ID;
+    }
+    break;
+
+  case SAI_SWITCH_ATTR_SRC_MAC_ADDRESS:
+    memcpy(sw->src_mac_address, attr->value.mac, 6);
+    break;
+
+  default:
+    fprintf(stderr, "switch attribute %d is not yet supported\n", attr->id);
+    return SAI_STATUS_NOT_SUPPORTED;
+  }
+
   return SAI_STATUS_SUCCESS;
 }
 
@@ -78,9 +129,12 @@ sai_status_t impl_initialize_switch(_In_ sai_switch_profile_id_t profile_id,
     // to be added for other event types
   }
 
+  sw = (struct __switch*)malloc(sizeof(struct __switch));
+  sw->default_port_vlan_id = 1; // default value of for default vlan id
+
   // data plane
   global_plist = get_dev_list();
   set_ports_up(global_plist);
-  
+
   return SAI_STATUS_SUCCESS;
 }
---
+++ impl/impl_sai_util.h
@@ -0,0 +1,7 @@
+#if !defined(_IMPL_SAI_UTIL_H)
+
+#define _IMPL_SAI_UTIL_H
+
+#define vlan_id_range_ok(vlan_id) ((vlan_id)>=1 && (vlan_id)<=4095)
+
+#endif
